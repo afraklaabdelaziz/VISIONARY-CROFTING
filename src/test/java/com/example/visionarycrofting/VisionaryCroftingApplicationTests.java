29dc664680564459cd3fc6155d8bc764ea13f9ba
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-12-08 15:22:24 +0100

    ajouter l'archetecture de spring boot

---
+++ src/test/java/com/example/visionarycrofting/VisionaryCroftingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visionarycrofting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionaryCroftingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
